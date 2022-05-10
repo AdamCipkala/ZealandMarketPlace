02d8247cfdb565bdee381f483f32d789dea4d2fb
AdamCipkala <acipkala@gmail.com>
2022-05-10 11:16:07 +0200

    migration

---
+++ ZealandMarketPlace/Models/MarketPlaceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZealandMarketPlace.Models
+{
+    public class MarketPlaceDbContext : DbContext
+    {
+        public MarketPlaceDbContext()
+        {
+        }
+
+        public MarketPlaceDbContext(DbContextOptions<MarketPlaceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
