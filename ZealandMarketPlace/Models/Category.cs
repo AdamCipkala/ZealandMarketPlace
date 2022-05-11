21428c956e4192877dd48772a46a368ea7121b63
Jakub Cerovský <jaku1198@edu.zealand.dk>
2022-05-11 11:21:40 +0200

    Items

---
+++ ZealandMarketPlace/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZealandMarketPlace.Models
+{
+    public class Category
+    {
+    }
+}
