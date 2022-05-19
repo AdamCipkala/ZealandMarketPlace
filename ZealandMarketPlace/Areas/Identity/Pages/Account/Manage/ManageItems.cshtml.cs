55d224fade0b40fad8e151aab052cc9b196a093d
AdamCipkala <acipkala@gmail.com>
2022-05-19 13:51:31 +0200

    rebase

---
+++ ZealandMarketPlace/Areas/Identity/Pages/Account/Manage/ManageItems.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandMarketPlace.Models;
+using ZealandMarketPlace.Services.Interfaces;
+
+namespace ZealandMarketPlace.Areas.Identity.Pages.Account.Manage
+{
+    public class ManageItems : PageModel
+    {
+        private IItemService _itemService;
+        public IEnumerable<Item> Items { get; set; }
+       
+        public ManageItems(IItemService service)
+        {
+            _itemService = service;
+        }
+        
+        public void OnGet()
+        {
+            Items = _itemService.GetAllItems();
+        }
+    }
+}
