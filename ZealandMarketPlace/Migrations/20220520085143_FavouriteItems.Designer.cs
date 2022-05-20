0a3253de74293c1eaa45292397f065c53f5a282e
AdamCipkala <acipkala@gmail.com>
2022-05-20 10:52:35 +0200

    migration

--- ZealandMarketPlace/Migrations/20220520085143_FavouriteItems.Designer.cs
+++ ZealandMarketPlace/Migrations/20220520085143_FavouriteItems.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZealandMarketPlace.Models;
 
 namespace ZealandMarketPlace.Migrations
 {
     [DbContext(typeof(MarketPlaceDbContext))]
-    partial class MarketPlaceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220520085143_FavouriteItems")]
+    partial class FavouriteItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
