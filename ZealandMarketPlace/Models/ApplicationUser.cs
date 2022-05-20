509bc1929adfdbcee64284f6a411e07d9cda82c7
Jakub Cerovský <jaku1198@edu.zealand.dk>
2022-05-20 09:24:05 +0200

    Migraci prosim

--- ZealandMarketPlace/Models/ApplicationUser.cs
+++ ZealandMarketPlace/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace ZealandMarketPlace.Models
 {
@@ -6,6 +7,7 @@ namespace ZealandMarketPlace.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public List<Item> FavouriteItems { get; set; }
     }
 }
 
